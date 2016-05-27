fd9a0e2dab7099548d2892d0ab9dc721e243031c
Benjie Songsong <benjiesongsong@gmail.com>
2016-05-27 12:15:14 -0400

    fixed a merge conflict

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,9 @@
 import angular from 'angular';
 
-
 import './app.core/index';
 import './app.user/index';
-<<<<<<< HEAD
 import './app.roommate/index'
-
-angular
-  .module('app',['app.core','app.user', 'app.roommate']);
-=======
 import './app.bills/index';
 
 angular
-  .module('app',['app.core','app.user','app.bills']);
->>>>>>> 48dde8c7b7de11fc88c5234d63b297836872c7a5
+  .module('app',['app.core','app.user','app.bills', 'app.roommate']);
