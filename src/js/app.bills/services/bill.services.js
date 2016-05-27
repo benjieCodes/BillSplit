135125070a75d2260b0344c25e6866deeb07e74f
Boyzie Mathis <boyzielee@gmail.com>
2016-05-27 14:57:49 -0400

    able to post bills. working properly

--- src/js/app.bills/services/bill.services.js
+++ src/js/app.bills/services/bill.services.js
@@ -1,13 +1,19 @@
-function BillService ($http, urlConstant) {
+function BillService ($http, urlConstant, $cookies) {
 
 
   this.add = add
 
   function add (bills){
+    let token = $cookies.get('token');
+    let config = {headers: {'X-AUTH-TOKEN': token}};
+
     console.log(bills);
+    $http.post(urlConstant.URL + './bills', bills, config).then((res)=>{
+      console.log(res);
+    });
   };
 
 }
 
-BillService.$inject = ['$http', 'urlConstant'];
+BillService.$inject = ['$http', 'urlConstant', '$cookies'];
 export { BillService };
